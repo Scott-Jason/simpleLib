995f5d07e9705bd8a820e3f87e8eb759bbe82d7b
Jason Scott <jcpoems@gmail.com>
2024-07-06 14:25:00 -0400

    playing with objects

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
 console.log("hi man");
+
+const myLibrary = [];
+
+function Book(title, author, pageNum, read){
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.read = read;
+}
+
+function addBook(){
+    let userInput =  prompt("Please add a book in format 'title, author, #ofpages, read(true/false)'");
+    userInput = userInput.split(' ');
+    const aBook =  new Book(userInput[0],userInput[1],userInput[2],userInput[3]);
+    myLibrary.push(aBook);
+}
+
+addBook();
+console.log(myLibrary);
