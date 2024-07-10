6f350ad9b59bcba7fbf23305656680f7aabb7a45
Jason Scott <jcpoems@gmail.com>
2024-07-10 01:49:40 -0400

    Add books implementation added

--- javascript.js
+++ javascript.js
@@ -2,15 +2,29 @@ console.log("hi man");
 
 const myLibrary = [];
 
-function Book(title, author, pageNum, read){
+function Book(title, author, pageNum, read, index){
     this.title = title;
     this.author = author;
     this.pageNum = pageNum;
     this.read = read;
+    this.index = index;
 }
 
-myLibrary.push(new Book("TheHobbit", "JRR", "513", "true"));
-myLibrary.push(new Book("Leaves", "Nature", "5133", "true"));
+// Person.prototype.sayName = function() {
+//     console.log(`Hello, I'm ${this.name}!`);
+//   };
+
+Book.prototype.deleteSelf = function() {
+ //mnake this
+};
+
+Book.prototype.swapRead = function() {
+    //make this
+};
+
+myLibrary.push(new Book("The Hobbit", "JRR", "1084", "true", 0));
+myLibrary.push(new Book("Leaves Of Grass", "WW", "694", "false", 1));
+let counter = 2;
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".newBook");
@@ -26,7 +40,24 @@ closeButton.addEventListener("click", () => {
   dialog.close();
 });
 
-function addBook(){
+const form = document.querySelector("dialog form");
+
+form.addEventListener("submit",  function(e){
+    e.preventDefault();
+    console.log('yo momma');
+    const formData = new FormData(form);
+    let trueFalse = "true";
+    if(formData.get('usr_read') !== 'on'){
+        trueFalse = "false";
+        console.log("PLEASE");
+    }
+    myLibrary.push(new Book(formData.get('usr_title'), formData.get('usr_author'), formData.get('usr_pages'), trueFalse, counter));
+    displayBooks(myLibrary.length-1);
+    counter++;
+
+});
+
+function addBook(){ //pretty much depreceated
     let userInput =  prompt("Please add a book in format 'title, author, #ofpages, read(true/false)'");
     userInput = userInput.split(' ');
     const aBook =  new Book(userInput[0],userInput[1],userInput[2],userInput[3]);
